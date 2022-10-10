9df75bc727eec226e37c108e06a167ce77f0cfed
turtlemoji <turtlemoji@protonmail.com>
2022-10-10 17:42:29 -0300

    feat: test for processDiff

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,10 @@
 // import * as process from 'process';
+// import * as core from '@actions/core';
 // import * as cp from 'child_process';
 // import * as path from 'path';
-import { expect, test } from '@jest/globals';
+import { expect, test, describe } from '@jest/globals';
+
+import { processDiff } from '../src/processing';
 
 // test('throws invalid number', async () => {
 //   const input = parseInt('foo', 10)
@@ -18,16 +21,65 @@ import { expect, test } from '@jest/globals';
 
 // shows how the runner will run a javascript action with env / stdout protocol
 // test('test runs', () => {
-//   process.env['INPUT_MILLISECONDS'] = '500'
-//   const np = process.execPath
-//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
+//   process.env['INPUT_MILLISECONDS'] = '500';
+//   const np = process.execPath;
+//   const ip = path.join(__dirname, '..', 'lib', 'main.js');
 //   const options: cp.ExecFileSyncOptions = {
-//     env: process.env
-//   }
-//   console.log(cp.execFileSync(np, [ip], options).toString())
-// })
+//     env: process.env,
+//   };
+//   const res = cp.execFileSync(np, [ip], options).toString();
+//   console.log(res);
+// });
 
-// TODO Add tests
-test('test', async () => {
-  expect(true).toBeTruthy();
+describe('Main tests', () => {
+  const leakPart = '3d22222b3b0e689193235279328527dbf71f6e79781beaded0b24fdc3e22cf6fc56e';
+
+  test('processDiff test logic should pass', () => {
+    const mockGoodDiff = `
+diff --git a/LICENSE b/LICENSE
+index a111aa1..222b2b2 222111
+--- a/LICENSE
++++ b/LICENSE
+@@ -1,22 +1,22 @@
++(The MIT License)
+-The MIT License (MIT)
+Copyright 2022 DeFi Wonderland
+    `;
+    const result = processDiff(mockGoodDiff);
+
+    expect(result.passed).toBeTruthy();
+  });
+
+  test('processDiff test logic should pass with old leak', () => {
+    const mockGoodDiff = `
+diff --git a/LICENSE b/LICENSE
+index a111aa1..222b2b2 222111
+--- a/LICENSE
++++ b/LICENSE
+@@ -1,22 +1,22 @@
++(The MIT License)
+-The MIT License (MIT)
+Copyright 2022 DeFi Wonderland
+leak=abcd${leakPart}
+    `;
+    const result = processDiff(mockGoodDiff);
+
+    expect(result.passed).toBeTruthy();
+  });
+
+  test('processDiff test logic should fail', () => {
+    const mockBadDiff = `
+diff --git a/LICENSE b/LICENSE
+index a111aa1..222b2b2 222111
+--- a/LICENSE
++++ b/LICENSE
+@@ -1,22 +1,22 @@
++leak=abcd${leakPart}
+-The MIT License (MIT)
+Copyright 2022 DeFi Wonderland
+      `;
+    const result = processDiff(mockBadDiff);
+
+    expect(result.passed).toBeFalsy();
+  });
 });
