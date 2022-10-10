e81eee4b748fc24287266cfa689deafc4050db3c
turtlemoji <turtlemoji@protonmail.com>
2022-10-10 18:38:31 -0300

    refactor: remove comments

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,35 +1,6 @@
-// import * as process from 'process';
-// import * as core from '@actions/core';
-// import * as cp from 'child_process';
-// import * as path from 'path';
 import { expect, test, describe } from '@jest/globals';
 
 import { processDiff } from '../src/processing';
-
-// test('throws invalid number', async () => {
-//   const input = parseInt('foo', 10)
-//   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-// })
-
-// test('wait 500 ms', async () => {
-//   const start = new Date()
-//   await wait(500)
-//   const end = new Date()
-//   var delta = Math.abs(end.getTime() - start.getTime())
-//   expect(delta).toBeGreaterThan(450)
-// })
-
-// shows how the runner will run a javascript action with env / stdout protocol
-// test('test runs', () => {
-//   process.env['INPUT_MILLISECONDS'] = '500';
-//   const np = process.execPath;
-//   const ip = path.join(__dirname, '..', 'lib', 'main.js');
-//   const options: cp.ExecFileSyncOptions = {
-//     env: process.env,
-//   };
-//   const res = cp.execFileSync(np, [ip], options).toString();
-//   console.log(res);
-// });
 
 describe('Main tests', () => {
   const leakPart = 'dbf71f6e79781beaded0b24fdc3e22cf6fc56e';
